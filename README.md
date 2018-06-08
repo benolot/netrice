89c203a7504a10bb8617847162883c2a03dc0acc
Sam Fry <benolot@gmail.com>
2018-06-09 00:23:37 +0100

    Added initial version of library

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # netrice
 Simple Single Element CSS Spinners/Loaders
+
+# TODO
+
+[ ] Github pages Website
+[ ] Add proper documentation for contributors
+[ ] Issue template
+[ ] Complete half circle spinner
+[ ] Make colours and sizing and speed controlable with classes
+
+`gulp build`
+
+`gulp watch`
+
+Loaders in `/loaders/`
+
+Animations in `_animations.scss`
+
+Variables in `_variables.scss`
+
+Prefix all files with `_`
+
+Requirements for PR:
+
+- 4 spaces (no tabs)
+- Correct file placement
+- Built distribution CSS
+- Provide transparent and non transparent versions where applicable
+- uses variables not hard coded
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+
+gulp.task('build', function () {
+    gulp.src(['scss/**/*.scss'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('dist/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch("scss/**/*.scss", ['build'])
+})
+
+gulp.task('default', function () {
+    gulp.watch("scss/**/*.scss", ['build']);
+});
